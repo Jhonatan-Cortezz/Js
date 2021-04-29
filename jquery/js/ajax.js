7c12487da55b7870a4698ee422d5c870201d5cb8
Jhonatan-Cortez <jhonatancortez708@gmail.com>
2021-04-29 15:19:40 -0600

    Metodo post de ajax

--- jquery/js/ajax.js
+++ jquery/js/ajax.js
@@ -10,4 +10,31 @@ $(document).ready(function(){
       $('#datos').append("<p>"+element.first_name+"</p>")
     })
   })
+
+  var usuarios = {
+    'name': 'Jhonatan',
+    'last_name': 'Cortez'
+  }
+
+  $.post("https://reqres.in/api/users", usuarios, function(res){
+    console.log(res);
+  })
+
+  $("#formulario").submit(function(e){
+
+    e.preventDefault()
+
+    var usuarios = {
+      'name': $('input[name="name"]').val(),
+      'last_name': $('input[name="lats_name"]').val()
+    }
+  
+    $.post($(this).attr("action"), usuarios, function(res){
+      console.log(res);
+    }).done(function(){
+      alert("usuario creado");
+    })
+
+    return false
+  })
 })
